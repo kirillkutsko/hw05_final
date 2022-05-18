0b63770cfbf31874f05ecaf879d7e6927f08ffc8
Кирилл Куцко <eads@mail.ru>
2022-05-18 18:35:44 +0300

    _

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -1,8 +1,15 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 
 
 class ViewTestClass(TestCase):
-    def test_error_page(self):
+    def test_error_page_url(self):
+        """Проверить доступность URL."""
         response = self.client.get('/nonexist-page/')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_error_page_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        response = self.client.get('/nonexist-page/')
         self.assertTemplateUsed(response, 'core/404.html')
